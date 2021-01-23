fb20450e20b4bcd307e0822eb7a847dd18c33e93
tom-elvidge <tomelvidge1999@gmail.com>
2021-01-23 14:55:42 +0000

    Added multiprocessing to preprocess.py

--- surface_models/preprocess.py
+++ surface_models/preprocess.py
@@ -2,21 +2,21 @@ import numpy as np
 import nibabel as nib
 from scipy.ndimage import zoom, median_filter
 import glob
+from multiprocessing import Pool
+import sys
 
-# Check data directory for MRIs.
-mri_paths = glob.glob("data/*.nii.gz")
-print("Found MRIs:\n{}\n".format("\n".join(mri_paths)))
+def preprocess(file_path):
+    # Redirect stdout to a logfile.
+    sys.stdout = open("temp/preprocess.log","w")
 
-# Preprocess each MRI.
-for mri_path in mri_paths:
     # Open MRI.
-    print("Opening {}...".format(mri_path))
-    nifti_file = nib.load("data/S1_contrast1.nii.gz")
+    print("Opening {}...".format(file_path))
+    nifti_file = nib.load(file_path)
     mri = nifti_file.get_fdata()
 
     # Linear interpolation (order=1) in all axis to double size (8-fold???).
-    print("Scaling...")
-    mri = zoom(mri, (2, 2, 2), order=1)
+    #print("Scaling...")
+    #mri = zoom(mri, (2, 2, 2), order=1)
 
     # Smooth with a median filter to preserve edges.
     print("Applying median filter...")
@@ -33,6 +33,15 @@ for mri_path in mri_paths:
     mri = ((mri - minval) / (maxval - minval))
 
     # Save as a new nifti file.
-    preprocessed_path = "surface_models/preprocessed/" + mri_path.split("\\")[1].split(".")[0] + "_preprocessed.nii"
-    nib.save(nib.Nifti1Image(mri, np.eye(4)), preprocessed_path)
-    print("Saved preprocessed MRI to {}.\n".format(preprocessed_path))
+    output_filepath = "surface_models/preprocessed/" + file_path.split("\\")[1].split(".")[0] + "_preprocessed.nii"
+    nib.save(nib.Nifti1Image(mri, np.eye(4)), output_filepath)
+    print("Saved preprocessed to {}.\n".format(output_filepath))
+
+if __name__ == "__main__":
+    # Check data directory for MRIs.
+    file_paths = glob.glob("data/*.nii.gz")
+    print("Found MRIs:\n{}\n".format("\n".join(file_paths)))
+
+    # Preprocess each MRI.
+    with Pool() as pool:
+        pool.map(preprocess, file_paths)
